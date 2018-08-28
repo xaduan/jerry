46fd3603ac687d1cc24395e4bc4304c0f485db95
aduan <xjr303053197@163.com>
2018-08-29 00:24:56 +0800

    fix

---
+++ src/components/HelloKitty.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">
+    hellokitty
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloKitty",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <HelloKitty/>
   </div>
 </template>
+<script>
+import HelloKitty from "../components/HelloKitty.vue";
+export default {
+  components: {
+    HelloKitty
+  }
+};
+</script>
