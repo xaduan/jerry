45651a85d5cebdd3cb17ace44b911261eb2215ec
aduan <xjr303053197@163.com>
2018-10-14 22:29:06 +0800

    vue-property-decorator study

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,30 +10,36 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Vue, Provide, Watch } from 'vue-property-decorator';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Axios from 'axios';
 export interface HomeState {
   homeState: string;
 }
 @Component({
-  data() {
-    return {
-      homeState: 'homeState'
-    };
-  },
-
   components: {
     HelloWorld
   }
 })
 export default class Home extends Vue {
+  @Provide()
   message: number = 0;
+  @Provide()
   number1: number = 1;
+  @Provide()
+  msg2: any = 1;
+  @Watch('child')
+  onChildChanged(val: string, oldVal: string) {}
   get SumNumber(): number {
     return this.number1 + this.message;
   }
   countState(): void {
     this.message++;
+  }
+  async mounted() {
+    await Axios.get('/api/getinfo').then(response => {
+      this.msg2 = response.data;
+    });
   }
 }
 </script>
