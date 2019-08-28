a0baf3372504b1220a32df40bf33f87da64d60a3
aduan <xjr303053197@163.com>
2019-08-28 14:37:42 +0800

    fixed vscode setting

--- src/components/HelloKitty.vue
+++ src/components/HelloKitty.vue
@@ -4,22 +4,18 @@
     {{ name }}
     我是请求的数据
     <div v-if="msg2">{{ msg2 }}</div>
-    <div 
-      v-else 
-      style="margin:0 auto;display:inline-block;"><Spin/></div>
-    <Button 
-      type="primary" 
-      ghost 
-      @click="callNewApi">点我请求自己的接口</Button> 
-   
+    <div v-else style="margin:0 auto;display:inline-block;">
+      <Spin/>
+    </div>
+    <Button type="primary" ghost @click="callNewApi">点我请求自己的接口</Button>
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex';
-import axios from 'axios';
+import { mapState } from "vuex";
+import axios from "axios";
 export default {
-  name: 'HelloKitty',
+  name: "HelloKitty",
   data() {
     return {
       msg2: null,
@@ -27,7 +23,7 @@ export default {
       number2: 2
     };
   },
-  computed: Object.assign(mapState(['name']), {
+  computed: Object.assign(mapState(["name"]), {
     haha() {
       return this.number1 + this.number2;
     }
@@ -37,11 +33,17 @@ export default {
   },
   methods: {
     getInfo() {
-      axios.get('/api/getinfo').then(response => {
+      axios.get("/api/getinfo").then(response => {
         this.msg2 = response.data;
       });
     },
-    callNewApi() {}
+    callNewApi() {
+      return (
+        <div>
+          <div /> 54asd
+        </div>
+      );
+    }
   }
 };
 </script>
